8fb4b87cdd5cf0fd2367f4b656222d2886ab235b
Amish Naidu <amhndu@gmail.com>
2026-07-15 11:42:34 +0530

    fish: untrack fish_variables

---
+++ .config/fish/conf.d/colors.fish
@@ -0,0 +1,4 @@
+# Non-default colors (rest of the scheme matches fish's built-in default theme).
+# Kept here rather than in universal fish_variables so it's tracked and machine-independent.
+set -g fish_color_command blue
+set -g fish_color_search_match white --background=brblack
